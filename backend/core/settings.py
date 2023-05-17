b2fc4453f04f296d1957b6fb8e7041d247b71d06
Patryk Adamowicz <patrykadam.dev@gmail.com>
2023-05-17 20:10:55 +0200

    fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-INSTANCE_CONNECTION_NAME = os.environ("INSTANCE_CONNECTION_NAME")
+INSTANCE_CONNECTION_NAME = os.environ.get("INSTANCE_CONNECTION_NAME")
 DATABASES = {"default": env.db()}
 
 if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
